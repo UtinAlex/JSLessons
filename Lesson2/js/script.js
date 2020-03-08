ae473a92579680f33d52ee6c5976f485ab7ab89b
Alexander Utin <utinalex01@gmail.com>
2020-03-08 23:04:31 +0400

    Commit3Lesson2

---
+++ Lesson2/js/script.js
@@ -0,0 +1,31 @@
+'use strict';
+let a = []+false+null+true;
+let y=1;
+let x=y=2;
+console.log(a);
+console.log(x);
+let a2 = [] + 1 + 2;
+console.log(a2);
+let a3 = "1"[0];
+console.log(a3);
+let a4 = 2 && 1 && null && 0 && undefined;
+console.log(a4);
+
+//alert(null || 2 && 3 || 4);
+
+let s = [1,2,3];
+let s2 = [1,2,3];
+console.log(s==s2);
+console.log(+ "Infinnity");
+
+if("яблоко" === "яблоко") {
+    console.log("1");
+}
+else{
+    console.log("0");
+}
+
+let w = 0 || "" || 2 || undefined || true || false;
+console.log(w);
+
+for ()
